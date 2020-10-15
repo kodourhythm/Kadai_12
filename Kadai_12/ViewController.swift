66d4f5fca07c8a43489ce7ac72329e1475748fe9
kodou <kodou.rhythm@gmail.com>
2020-10-15 19:01:44 +0900

    Initial Commit

---
+++ Kadai_12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kadai_12
+//
+//  Created by kodou on 2020/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
